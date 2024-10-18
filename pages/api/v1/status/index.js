88634af5e7c59d15540664fa3e62637c43ee33eb
Gustavo Resende <gustavo@resende.app>
2024-10-18 11:01:04 +0000

    retornando max connections e updated at (preliminar)

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,13 @@
 import database from "infra/database.js"
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;')
-  console.log(result.rows)
-  response.status(200).json({ "chave" : "valor" })
+  const updatedAt = new Date().toISOString();
+  const maxConnections = await database.query('show max_connections;')
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    max_connections: maxConnections.rows[0].max_connections
+  })
 }
 
 export default status
